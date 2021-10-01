3299e481bcbda90840e97847cedc4100990e8ddf
NicolasMCP <nicolas.mcp@gmail.com>
2021-09-30 23:35:46 -0300

    Primeiros scripts

---
+++ Mundo1-PrimeirosPassos/src/PrimeirosPassos001.py
@@ -0,0 +1,4 @@
+nome = input('Qual o seu nome ')
+idade = input('Digite sua idade ')
+peso = input('Digite o seu peso ')
+print('\nNome:', nome, '\nIdade:', idade, '\nPeso:', peso)
---
+++ Mundo1-PrimeirosPassos/src/PrimeirosPassosEx01.py
@@ -0,0 +1,2 @@
+nome = input('Por favor, digite o seu nome ')
+print('Seja bem vindo(a) ' + nome + '!')
